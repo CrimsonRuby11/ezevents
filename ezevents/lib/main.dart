4e0195c7f78fc76359dfd5a645d86c38c879f401
CrimsonRuby11 <skprithviraj8@gmail.com>
2025-01-24 11:27:16 +0530

    n

--- ezevents/lib/main.dart
+++ ezevents/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ezevents/pages/mainpage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,8 +14,10 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromRGBO(101, 150, 112, 1),
-        ),
+            seedColor: Color.fromRGBO(101, 150, 112, 1),
+            primary: Color.fromRGBO(101, 150, 112, 1),
+            secondary: Color.fromRGBO(141, 129, 103, 1)),
+        textTheme: GoogleFonts.montserratTextTheme().copyWith(),
       ),
       home: MainPage(),
     );
--- ezevents/lib/pages/mainpage.dart
+++ ezevents/lib/pages/mainpage.dart
@@ -8,11 +8,115 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  bool isEvents = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: Text(
+          "Welcome, User!",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
       body: SafeArea(
-        child: Container(),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // NOTIFICATIONS
+              Container(
+                padding: const EdgeInsets.all(12),
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondaryContainer,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      "Notifications",
+                      style: TextStyle(fontSize: 25),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "• Sarojini Naidu Hall is Under maintenance!",
+                      style: TextStyle(fontSize: 15),
+                    )
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              // DASHBOARD
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      "Dashboard",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    !isEvents
+                        ? Text("Send events for approval to see them here!")
+                        : Container(
+                            height: 300,
+                            child: ListView.builder(
+                              itemCount: 10,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) =>
+                                  EventTile(index: index),
+                            ),
+                          )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EventTile extends StatelessWidget {
+  final int index;
+
+  const EventTile({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "Event Title: Event $index",
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text("Date: Sunday 10/10/2025")
+          ],
+        ),
       ),
     );
   }
